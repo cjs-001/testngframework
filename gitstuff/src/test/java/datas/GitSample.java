14706b23ba11daa077928dcda5d4d5953dfb72db
cjs-001 <charanjs1998@gmail.com>
2024-01-15 20:47:21 +0530

    asd

--- gitstuff/src/test/java/datas/GitSample.java
+++ gitstuff/src/test/java/datas/GitSample.java
@@ -6,19 +6,8 @@ public class GitSample {
 		// TODO Auto-generated method stub
 
 		
-		System.out.println("gitsample");
-		System.out.println("gitsample");
-		
-		System.out.println("gitsample");
-		System.out.println("gitsample");
-		
-		System.out.println("gitsample");
-		System.out.println("gitsample");
-		System.out.println("gitsample");
-		System.out.println("gitsample");
-		
-		System.out.println("develop");
-		System.out.println("develop");
+		int[] i= {0,1,2,3,4,5,6,7,8};
+		String[] asd = {"asd","qwe","zxc"};
 	}
 
 }
